1f04c715d8035a98566c28e810b1540bd027b0a9
Shaun Edwards <shaun.edwards@gmail.com>
2014-08-30 22:07:45 -0400

    Improved robustness of mkdir function by catching specific error when directory exists

--- industrial_robot_pkg_gen/src/industrial_robot_pkg_gen/utils.py
+++ industrial_robot_pkg_gen/src/industrial_robot_pkg_gen/utils.py
@@ -1,12 +1,15 @@
 
 import os
+import errno
 
 def mkdir(path):
   try:
     os.mkdir(path)
-
-  except OSError:
-    print("Directory: " + path + " already exists, ignoring")
+  except OSError as ex:
+    if ex.errno == errno.EEXIST and os.path.isdir(path):
+      print("Directory: " + path + " already exists, ignoring")
+      pass
+    else: raise
 
 def touch(path):
   open(path, 'a').close()
